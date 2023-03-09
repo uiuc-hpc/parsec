4f42296ec442769b37fa4a97580c84b096e844f7
Omri Mor <omrimor2@illinois.edu>
2023-03-09 13:03:45 -0800

    recursive: complete task on local execution stream

--- parsec/recursive.h
+++ parsec/recursive.h
@@ -16,7 +16,6 @@ typedef struct parsec_recursive_cb_data_s parsec_recursive_cb_data_t;
 typedef void (*parsec_recursive_callback)(parsec_taskpool_t*, const parsec_recursive_cb_data_t* );
 
 typedef struct parsec_recursive_cb_data_s {
-    parsec_execution_stream_t    *es;
     parsec_task_t                *task;
     parsec_recursive_callback     callback;
     int nbdesc;
@@ -27,10 +26,11 @@ static inline int parsec_recursivecall_callback(parsec_taskpool_t* tp, void* cb_
 {
     int i, rc = 0;
     parsec_recursive_cb_data_t* data = (parsec_recursive_cb_data_t*)cb_data;
+    parsec_execution_stream_t *es = parsec_my_execution_stream();
 
     /* call user callback *before* we complete and release the task */
     data->callback( tp, data );
-    rc = __parsec_complete_execution(data->es, data->task);
+    rc = __parsec_complete_execution(es, data->task);
 
     for( i = 0; i < data->nbdesc; i++ ) {
         parsec_tiled_matrix_dc_destroy( (parsec_tiled_matrix_dc_t*)(data->desc[i]) );
@@ -42,8 +42,7 @@ static inline int parsec_recursivecall_callback(parsec_taskpool_t* tp, void* cb_
 }
 
 static inline int
-parsec_recursivecall( parsec_execution_stream_t    *es,
-                      parsec_task_t                *task,
+parsec_recursivecall( parsec_task_t                *task,
                       parsec_taskpool_t            *tp,
                       parsec_recursive_callback     callback,
                       int nbdesc,
@@ -57,7 +56,6 @@ parsec_recursivecall( parsec_execution_stream_t    *es,
 
     /* Callback */
     cbdata = (parsec_recursive_cb_data_t *) malloc( sizeof(parsec_recursive_cb_data_t) + (nbdesc-1)*sizeof(parsec_data_collection_t*));
-    cbdata->es       = es;
     cbdata->task     = task;
     cbdata->callback = callback;
     cbdata->nbdesc   = nbdesc;
@@ -72,7 +70,7 @@ parsec_recursivecall( parsec_execution_stream_t    *es,
     parsec_taskpool_set_complete_callback( tp, &parsec_recursivecall_callback,
                                            (void *)cbdata );
 
-    parsec_context_add_taskpool( es->virtual_process->parsec_context, tp);
+    parsec_context_add_taskpool( task->taskpool->context, tp );
 
     return -1;
 }
